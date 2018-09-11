696ff65dfc137782fc052f7a98318a1509fdcee7
Yang Chen <young.y.chen@gmail.com>
2018-09-11 10:34:28 -0700

    add shim.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import './global';
-
+import './shim.js';
 import Chain3 from 'chain3';
 
 export default class App extends React.Component {
